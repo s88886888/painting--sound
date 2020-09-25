a2444f1296f5a453089b1d006331cd267290be54
Linson <854700937@qq.com>
2020-09-25 12:07:30 +0800

    优化框架

--- PaintingAndSound/PaintingAndSound.DataAccess/Services/IRadioRepository.cs
+++ PaintingAndSound/PaintingAndSound.DataAccess/Services/IRadioRepository.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using PaintingAndSound.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PaintingAndSound.DataAccess.Services
 {
-    public interface IRadioRepository
+    public interface IRadioRepository:IEntityRepository<Radio>
     {
     }
 }
