c54babdff9fa996d981f8c982a426c0cc2244d73
Linson <854700937@qq.com>
2020-10-25 11:55:55 +0800

    完善业务

---
+++ PaintingAndSound/PaintingAndSound.WebAPI/Controllers/RadioMusicController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PaintingAndSound.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RadioMusicController : ControllerBase
+    {
+        // GET: api/<RadioMusicController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<RadioMusicController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RadioMusicController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RadioMusicController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RadioMusicController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- PaintingAndSound/PaintingAndSound.WebAPI/Startup.cs
+++ PaintingAndSound/PaintingAndSound.WebAPI/Startup.cs
@@ -55,16 +55,26 @@ namespace PaintingAndSound.WebAPI
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
                     Title = "画声博客Api这是个小项目！请不要DD攻击！",//标题
                     Version = "v1", //版本
-                     Description = $"Linson 画声博客API v1",    //描述
+                    Description = $"Linson 画声博客API v1",    //描述
                     Contact = new OpenApiContact { Name = "Linson", Email = "", Url = new Uri("https://gitee.com/S88888888") },
                     License = new OpenApiLicense { Name = "Linson许可证", Url = new Uri("https://gitee.com/S88888888/painting--sound") }
                 });
-                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+
+                //在发布时，XML不可使用
+
+                //var basePath = Microsoft.DotNet.PlatformAbstractions.ApplicationEnvironment.ApplicationBasePath;
+                //// 这个就是刚刚配置的xml文件名
                 //var xmlPath = Path.Combine(basePath, "PaintingAndSound.WebAPI.xml");
-                //c.IncludeXmlComments(xmlPath);
+                //// 默认的第二个参数是false，这个是controller的注释，记得修改
+                //c.IncludeXmlComments(xmlPath, true);
+
+                //var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+                //var xmlPath = Path.Combine(basePath, "PaintingAndSound.WebAPI.xml");//这个就是刚刚配置的xml文件名
+                //c.IncludeXmlComments(xmlPath, true); // 这个是controller的注释
             });
 
 
